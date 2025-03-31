e5c0afbb737eb66bc60283af323185f246589e77
Josse Van Delm <josse.vandelm@gmail.com>
2025-03-31 17:59:11 +0200

    Add torchvision example properly

--- examples/torchvision_resnet.py
+++ examples/torchvision_resnet.py
@@ -1,11 +1,11 @@
 import torch
-import torchvision.models as models
+from torchvision import models
 from torch_mlir import fx
+from frontend_mess.postprocess.torch import nullify_dense_resources
 
 
 if __name__ == "__main__":
-
-    resnet18 = models.resnet18(pretrained=True).eval()
+    resnet18 = models.resnet18(weights=models.ResNet18_Weights.DEFAULT).eval()
     example_args = torch.ones(1, 3, 224, 224)
 
     module = fx.export_and_import(
@@ -14,3 +14,4 @@ if __name__ == "__main__":
         output_type="linalg-on-tensors",
         func_name=resnet18.__class__.__name__,
     )
+    print(nullify_dense_resources(str(module)))
