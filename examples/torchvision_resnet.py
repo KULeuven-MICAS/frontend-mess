03d8589b680966981e8c8121d2b3755e82aea913
Josse Van Delm <josse.vandelm@gmail.com>
2026-04-24 14:40:51 +0200

    Parse resources

--- examples/torchvision_resnet.py
+++ examples/torchvision_resnet.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
         output_type="linalg-on-tensors",
         func_name=resnet18.__class__.__name__,
     )
-    print(nullify_dense_resources(str(module)))
+    print(str(module))
--- examples/transformers_berttiny.py
+++ examples/transformers_berttiny.py
@@ -23,4 +23,4 @@ if __name__ == "__main__":
         output_type="linalg-on-tensors",
         func_name=model.__class__.__name__,
     )
-    print(nullify_dense_resources(str(module))) 
+    print(str(module))
